a33c7b7ad67568006a58f1b415b90c2c83b71c7d
Michau <michalgorazs3@gmail.com>
2021-07-09 22:32:54 +0200

    test- konfiguracje

---
+++ src/Controller/ConferenceController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ConferenceController extends AbstractController
+{
+//    #[Route('/', name: 'homepage')]
+    #[Route("/hello/{name}", name: "homepage")]
+
+    public function index(string $name = 'Michal'): Response
+    {
+//        return $this->json([
+//            'message' => 'Welcome to your new controller!',
+//            'path' => 'src/Controller/ConferenceController.php',
+//        ]);
+
+        $greet = '';
+        if($name){
+            $greet = sprintf('<h1>Cześć %s!</h1>', htmlspecialchars($name));
+        }
+
+        return new Response(<<<EOF
+        <html>
+         <body>
+            <p>Otwarcie wkrótce</p>
+            $greet
+         </body>
+        </html>
+        EOF
+        );
+
+    }
+}
